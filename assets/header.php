f0182268e32d2a53c13f160d71e37ebdeea03a8a
eslamNasser <eslamnasser@hmaserv.net>
2016-12-07 18:04:12 +0200

    First commit

---
+++ assets/header.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="keywords" content="your,keywords,here,comma,separated,no,spaces">
+        <meta name="description" content="150 chars">
+        <meta name="subject" content="your website's subject">
+        <meta name="language" content="en">
+        <meta name="robots" content="index,follow">
+        <meta name="revised" content="Sunday, July 18th, 2010, 5:15 pm">
+        <meta name="abstract" content="">
+        <meta name="topic" content="">
+        <meta name="summary" content="">
+        <meta name="classification" content="business">
+        <meta name="author" content="name, email@example.com">
+        <meta name="designer" content="">
+        <meta name="reply-to" content="email@example.com">
+        <meta name="owner" content="">
+        <meta name="url" content="">
+        <meta name="identifier-URL" content="">
+        <meta name="directory" content="submission">
+        <meta name="category" content="">
+        <meta name="coverage" content="Worldwide">
+        <meta name="distribution" content="Global">
+        <meta name="rating" content="General">
+        <meta name="referrer" content="never">
+        <meta name="revisit-after" content="7 days">
+        <meta http-equiv="refresh" content="">
+        <!-- Cache Control -->
+        <meta http-equiv="Expires" content="0">
+        <meta http-equiv="Pragma" content="no-cache">
+        <meta http-equiv="Cache-Control" content="no-cache">
+        <title>Starter template</title>
+
+        <link rel="stylesheet" href="css/output/style.css">
+    </head>
+    <body>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include 'assets/header.php' ?>
+
+<h1>TEST</h1>
+
+<?php include 'assets/footer.php' ?>
